821a7e770028e575776ea7c7b06f7d034607cca4
homuler <eulerdora.1810.mann@gmail.com>
2016-03-20 02:24:14 +0900

    [Modify] change the directory structures
    - delete unused files
    - move server scripts

--- examples/serviceWorker/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/examples/serviceWorker/sw.js').then((registration) => {
-    console.log('ServiceWorker registration successful: ', registration);
-  }).catch((err) => {
-    console.log('error ', err);
-  });
-}
